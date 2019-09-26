beb584d06409653732036e66bc64a913e2763c44
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2019-09-26 13:50:37 +0200

    registration fix

--- GIRAFFE/code/MasterScript.m
+++ GIRAFFE/code/MasterScript.m
@@ -13,11 +13,16 @@ tvm_smoothFunctionals(configuration);
 
 configuration = [];
 configuration.i_FunctionalFolder = 'Scans/Functional/Realigned';
-configuration.o_DesignMatrix = 'DesignMatrix/design1.mat';
-tvm_design_empty(configuration);
+configuration.o_DesignMatrix = 'DesignMatrix/design_init.mat';
+tvm_design_initialise(configuration);
 
 configuration = [];
-configuration.i_DesignMatrix = 'DesignMatrix/design1.mat';
+configuration.i_DesignMatrix = 'DesignMatrix/design_init.mat';
+configuration.o_DesignMatrix = 'DesignMatrix/design_mean.mat';
+tvm_design_constant(configuration);
+
+configuration = [];
+configuration.i_DesignMatrix = 'DesignMatrix/design_mean.mat';
 configuration.i_Stimulus = {'Stimulus/STIM_RightOn.mat', 'Stimulus/STIM_RightOff.mat', 'Stimulus/STIM_LeftOn.mat', 'Stimulus/STIM_LeftOff.mat'};
 configuration.i_HrfParameters = [5, 10, 0.5, 0.5, 2, 0, 32];
 configuration.i_Labels = {'Stimulus_{Right,On}', 'Stimulus_{Right,Off}', 'Stimulus_{Left,On}', 'Stimulus_{Left,Off}'};
@@ -25,53 +30,48 @@ configuration.i_TR = 3.06;
 configuration.i_TemporalDerivative = false;
 configuration.i_DispersionDerivative = false;
 configuration.i_DiagonaliseElements = true;
-configuration.o_DesignMatrix = 'DesignMatrix/design2.mat';
+configuration.o_DesignMatrix = 'DesignMatrix/design_stim.mat';
 tvm_design_stimulus(configuration);
 
 configuration = [];
-configuration.i_DesignMatrix = 'DesignMatrix/design2.mat';
-configuration.o_DesignMatrix = 'DesignMatrix/design3.mat';
-tvm_design_constant(configuration);
-
-configuration = [];
-configuration.i_DesignMatrix = 'DesignMatrix/design3.mat';
+configuration.i_DesignMatrix = 'DesignMatrix/design_stim.mat';
 configuration.i_Deletions = repmat({1:3}, 1, 18);
-configuration.o_DesignMatrix = 'DesignMatrix/basic_design.mat';
+configuration.o_DesignMatrix = 'DesignMatrix/basic_remove_vols.mat';
 tvm_design_removeVolumes(configuration);
 
 configuration = [];
-configuration.i_DesignMatrix = 'DesignMatrix/basic_design.mat';
+configuration.i_DesignMatrix = 'DesignMatrix/basic_remove_vols.mat';
 configuration.i_CutOffFrequency = 1/100;
 configuration.i_TR = 3.06;
-configuration.o_DesignMatrix = 'DesignMatrix/design4.mat';
+configuration.o_DesignMatrix = 'DesignMatrix/design_filtered.mat';
 tvm_design_highpassFilter(configuration);
 
 configuration = [];
-configuration.i_DesignMatrix = 'DesignMatrix/design4.mat';
+configuration.i_DesignMatrix = 'DesignMatrix/design_filtered.mat';
 configuration.i_MotionFiles = 'Scans/Functional/Realigned/rp*.txt';
-configuration.o_DesignMatrix = 'DesignMatrix/DesignStimulus.mat';
+configuration.o_DesignMatrix = 'DesignMatrix/design_motion.mat';
 tvm_design_motionRegression(configuration);
 
 configuration = [];
-configuration.i_DesignMatrix = 'DesignMatrix/DesignStimulus.mat';
+configuration.i_DesignMatrix = 'DesignMatrix/design_motion.mat';
 configuration.i_PhysioFiles = 'Scans/PhysioLog/Resp*.mat';
 configuration.i_ScanTriggers = 'Scans/TimeStamps/Run*';
 configuration.i_Order = 5;
 configuration.i_Type = 'Respiration';
-configuration.o_DesignMatrix = 'DesignMatrix/DesignStimulus.mat';
+configuration.o_DesignMatrix = 'DesignMatrix/design_resp.mat';
 tvm_design_retroicor(configuration);
 
 configuration = [];
-configuration.i_DesignMatrix = 'DesignMatrix/DesignStimulus.mat';
+configuration.i_DesignMatrix = 'DesignMatrix/design_resp.mat';
 configuration.i_PhysioFiles = 'Scans/PhysioLog/Puls*.mat';
 configuration.i_ScanTriggers = 'Scans/TimeStamps/Run*';
 configuration.i_Order = 5;
 configuration.i_Type = 'Heartbeat';
-configuration.o_DesignMatrix = 'DesignMatrix/DesignStimulus.mat';
+configuration.o_DesignMatrix = 'DesignMatrix/Design.mat';
 tvm_design_retroicor(configuration);
 
 configuration = [];
-configuration.i_DesignMatrix = 'DesignMatrix/DesignStimulus.mat';
+configuration.i_DesignMatrix = 'DesignMatrix/Design.mat';
 configuration.i_ReferenceVolume = 'Scans/Functional/MeanFunctional.nii';
 configuration.i_SourceDirectory = 'Scans/Functional/Smoothed/';
 configuration.i_Mask = 'RegionsOfInterest/Brain.nii';
@@ -114,11 +114,20 @@ tvm_volumeWithBoundariesToMovie(configuration);
 configuration = [];
 configuration.i_ReferenceVolume = 'Scans/Functional/MeanFunctional.nii';
 configuration.i_Boundaries = 'Boundaries/bbregister.mat';
+configuration.i_MinimumVoxels = 4;
+configuration.i_MinimumVertices = 100;
+configuration.i_CuboidElements = true;
+configuration.i_Tetrahedra = true;
+configuration.i_NeighbourSmoothing = 0.1;
+configuration.i_Mode = 'syty';
+configuration.i_ReverseContrast = true;
+configuration.i_OptimisationMethod = 'GreveFischl';
+configuration.i_ContrastMethod = 'average';
 configuration.o_Boundaries = 'Boundaries/rbr.mat';
+configuration.o_DisplacementMap = 'Boundaries/Displacement.nii';
 tvm_recursiveBoundaryRegistration(configuration);
 
 configuration = [];
-configuration.i_ReferenceVolume = 'Scans/Functional/MeanFunctional.nii';
 configuration.i_Boundaries = 'Boundaries/rbr.mat';
 configuration.i_Axis = 'transversal';
 configuration.o_RegistrationMovie = 'Coregistration/RBR.avi';
@@ -131,7 +140,6 @@ configuration.o_ObjPial = 'LevelSets/?h.pial.obj';
 tvm_boundariesToObj(configuration);
 
 configuration = [];
-configuration.i_ReferenceVolume = 'Scans/Functional/MeanFunctional.nii';
 configuration.i_ObjWhite = 'LevelSets/?h.white.obj';
 configuration.i_ObjPial = 'LevelSets/?h.pial.obj';
 configuration.o_SdfWhite = 'LevelSets/?h.white.sdf.nii';
@@ -183,18 +191,18 @@ tvm_thresholdVolumes(configuration);
 configuration = [];
 configuration.i_ROI = {'RegionsOfInterest/StimulusActivation.nii'};
 configuration.i_Layers = 'LevelSets/brain.layers.nii';
-configuration.o_DesignMatrix = {'DesignMatrix/StimulusActivation.mat'};;
+configuration.o_DesignMatrix = {'DesignMatrix/StimulusActivation.mat'};
 tvm_roiToDesignMatrix(configuration);
 
 configuration = [];
-configuration.i_DesignMatrix = 'DesignMatrix/DesignStimulus.mat';
+configuration.i_DesignMatrix = 'DesignMatrix/Design.mat';
 configuration.i_FunctionalFolder = 'Scans/Functional/Realigned';
 configuration.i_Confounds = {'Respiration', 'Heartbeat', 'Motion', 'Filter'};
 configuration.o_FilteredFolder = 'Scans/Functional/Filtered/';
 tvm_regressConfounds(configuration);
 
 configuration = [];
-configuration.i_DesignMatrix = {'DesignMatrix/StimulusActivation.mat'};;
+configuration.i_DesignMatrix = {'DesignMatrix/StimulusActivation.mat'};
 configuration.i_FunctionalFolder = 'Scans/Functional/Filtered/';
 configuration.i_RegressionApproach = 'OLS';
 configuration.o_TimeCourse = {'TimeCourses/StimulusActivation.mat'};;
@@ -209,7 +217,7 @@ configuration.o_FMap = {'Activation/F_Respiration.nii', 'Activation/F_Heartrate.
 tvm_glmToFMap(configuration);
 
 configuration = [];
-configuration.i_DesignMatrix = 'DesignMatrix/DesignStimulus.mat';
+configuration.i_DesignMatrix = 'DesignMatrix/Design.mat';
 configuration.o_Image = 'DesignMatrix/design.fig';
 tvm_design_saveToImage(configuration);
 
